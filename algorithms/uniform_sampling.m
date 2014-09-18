147f315e5c77e9c9869edd504f662f951dcb16cb
Simon Shaolei Du <simonshaoleidu@gmail.com>
2014-09-17 17:40:24 -0700

    refactor uniform sampling
    change the interface

--- algorithms/uniform_sampling.m
+++ algorithms/uniform_sampling.m
@@ -5,48 +5,54 @@ function out = uniform_sampling(in)
 % - A, a matrix
 % - k, the target rank of the approximation
 % - p, the rank of first two partition matrix
-% - number_of_c_and_r, a two row matrix specifying the numbers of column and rows 
-%   samples to use   
+% - c, number of columns to select
+% - r, number of rows to select
 % - q, the number of times to repeat each Nystrom method for each number of
 %  column samples
 %
 % out is a structure with the following fields:
-%  - cidx, index vector of selected columns
-%  - ridx, index vector of selected rows
-%  - specerr, froerr, trerr: each matrices with two rows representing 
-%  the spectral, frobenius, 
-%  and trace norms of the errors in using q realizations of the simple 
-%  column-based (sampled uniformly at random w/o replacement)
-%  Nystrom extension to A, using l columns. The first row corresponds to 
-%  Nystrom extensions where the rank was not fixed, the second to Nystrom
-%  extensions where the rank was fixed
-%  - sigma_k, the k-th singular value of A
-% 
-%  -timings, a two row matrix of the time it took to run each experiment 
-% (i.e. form C, Winv, Wkinv), including the time to approximate the leverage scores
-
-% % if testing
-%out = dummy_extension(in);
-%return
+%  - cidx, c*q matrix represents the column index we choose for each
+%  iteration
+%  - ridx, r*q matrix represents the row index we choose for each
+%  iteration
+%  - sigma_k: 1*q vector represents the kth singular value of
+%  reconstruction matrix for each iteration
+%  - froerr: 1*q vector represents the error in frobenius norm of
+%  reconstruction matrix for each iteration
+%  - froerr_k: 1*q vector represents the error in frobenius norm of
+%  truncated rank-k reconstruction matrix for each iteration
+%  - specerr: 1*q vector represents the error in spectral norm of
+%  reconstruction matrix for each iteration
+%  - specerr_k: 1*q vector represents the error in spectral norm of
+%  truncated rank-k reconstruction matrix for each iteration
+%  - construct_time: 1*q vector represents the time to choose columns and rows
+%  - metric_computing_time: 1*q vector represents the time to compute
+%  different metrics
 
 
 [m,n] = size(in.A);
-c = in.number_of_c_and_r(1);
-r = in.number_of_c_and_r(2);
-out.specerr = zeros(2,in.q);
-out.froerr = zeros(2,in.q);
-out.trerr = zeros(2,in.q);
-out.timings = zeros(2,in.q);
+
+out.cidx = zeros(c,in.q);
+out.ridx = zeros(r,in.q);
+
+out.sigma_k = zeros(1,in.q);
+out.froerr = zeros(1,in.q);
+out.froerr_k = zeros(1,in.q);
+out.specerr = zeros(1,in.q);
+out.specerr_k = zeros(1,in.q);
+
+out.construct_time = zeros(1,q);
+out.metric_computing_time = zeros(1,q);
 
 for iter=1:in.q
     tic
-        p = randperm(n);
-        cidx = p(1:c);
+        permutation = randperm(n);
+        out.cidx(:,iter) = permutation(1:c);
         C = in.A(:,cidx);
-        p = randperm(m);
-        ridx = p(1:r);
+        permutation = randperm(m);
+        out.ridx(:,ridx) = permutation(1:r);
         R = in.A(ridx,:);
-    out.timings(1, iter) = toc;
+    out.construct_time(1,iter) = toc;
     
     tic
         [Qc,~] = qr(C,0);
@@ -61,14 +67,16 @@ for iter=1:in.q
         residual = in.A-CUR;
         residual_k = in.A - CUR_k;
     
-        out.specerr(1,iter) = svds(residual,1);
-        out.specerr(2,iter) = svds(residual_k,1);
+        out.sigma_k(1,iter) = Sb(end,end);
         out.froerr(1,iter) = norm(residual,'fro');
-        out.froerr(2,iter) = norm(residual_k,'fro');
+        out.froerr_k(1,iter) = norm(residual_k,'fro');
+        out.specerr(1,iter) = svds(residual,1);
+        out.specerr_k(1,iter) = svds(residual_k,1);
+        
         %out.trerr(1,iter) = trace(sqrt(residual*residual'));
         %out.trerr(2,iter) = trace(sqrt(residual_k*residual_k'));
-        out.sigma_k = Sb(end,end);
-    out.timings(2,iter) = toc;
+        
+    out.metric_computing_time(1,iter) = toc;
 
 end
 
