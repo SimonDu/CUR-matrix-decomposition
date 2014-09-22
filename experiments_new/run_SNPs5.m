f9931137681a1e5f9d5dfac7dc5a8f9d5cfb5ed5
Simon Shaolei Du <simonshaoleidu@gmail.com>
2014-09-21 21:53:32 -0700

    fix bugs

--- experiments_new/run_SNPs5.m
+++ experiments_new/run_SNPs5.m
@@ -3,22 +3,22 @@ A = A(1:(end-3), 1:size(A,2)-3); % The last three columns and rows are all 0s
 in.A = normalize_kernel_data(A);
 clear A;
 
-in.k = 10;
+in.k = 5;
 in.c = floor(6*in.k*log(in.k));
 in.r = floor(6*in.k*log(in.k));
 in.q = 10;
 
 methods = {'subspace_expected','deterministic'};
-p_values = (in.j:floor(6*in.k*log(in.k))-1);
+p_values = (5:floor(6*in.k*log(in.k))-1);
 
 out = run_dataset_different_p(in,methods,p_values);
 
-save('./output/SNPs10')
+save('./output/SNPs5')
 
 p_values_plot_deterministic;
-export_fig(gcf,'./plots/p_plots_SNPs10_deterministic.pdf');
+export_fig(gcf,'./plots/p_plots_SNPs5_deterministic.pdf');
 close all;
 
 p_values_plot_subspace_expected;
-export_fig(gcf,'./plots/p_plots_SNPs10_subspace_expected.pdf');
+export_fig(gcf,'./plots/p_plots_SNPs5_subspace_expected.pdf');
 close all;
