e02ced7bb3676f7fc7efb3fbfbf3371453c82b96
Simon Shaolei Du <simonshaoleidu@gmail.com>
2014-09-21 20:16:05 -0700

    add compact

---
+++ experiments_new/run_abaloneCompact20_sigma_015.m
@@ -0,0 +1,28 @@
+cutoffmultiplier = 3;
+in.sigma = 0.15; 
+in.cutoff = cutoffmultiplier*in.sigma;
+in.d = 8;
+
+load 'abalone_distance_matrix'; 
+in.A = generate_compact_RBF_kernel(X, in.sigma, in.d, in.cutoff);
+clear X;
+
+in.k = 20;
+in.c = 80;
+in.r = 80;
+in.q = 10;
+
+methods = {'subspace_expected','deterministic'};
+p_values = (20:79);
+
+out = run_dataset_different_p(in,methods,p_values);
+
+save('./output/abaloneCompact20_sigma_015')
+
+p_values_plot_deterministic;
+export_fig(gcf,'./plots/p_plots_abaloneCompact20_sigma_015_deterministic.pdf');
+close all;
+
+p_values_plot_subspace_expected;
+export_fig(gcf,'./plots/p_plots_abaloneCompact20_sigma_015_subspace_expected.pdf');
+close all;
