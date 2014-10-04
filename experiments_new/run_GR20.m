92707eebed5e47d9a1df3cdde00abf13d61f6b96
Simon Shaolei Du <simonshaoleidu@gmail.com>
2014-10-04 00:37:11 -0700

    try p = less than k

--- experiments_new/run_GR20.m
+++ experiments_new/run_GR20.m
@@ -5,23 +5,23 @@ in.c = 80;
 in.r = 80;
 in.q = 15;
 
-methods = {'subspace_expected','deterministic'};
-p_values = (20:40);
+methods = {'subspace_expected'};
+p_values = (1:40);
 
-s = svds(in.A,p_values(end));
-plot(p_values,s(p_values(1):end)./s(p_values(1)));
-title('singular value decay');
-export_fig('./plots/decay_GR20.pdf');
-close all;
+% s = svds(in.A,p_values(end));
+% plot(p_values,s(p_values(1):end)./s(p_values(1)));
+% title('singular value decay');
+% export_fig('./plots/decay_GR20.pdf');
+% close all;
 
 
 out = run_dataset_different_p(in,methods,p_values);
 
-save('./output/GR20')
-
-p_values_plot_deterministic;
-export_fig(gcf,'./plots/p_plots_GR20_deterministic.pdf');
-close all;
+% save('./output/GR20')
+% 
+% p_values_plot_deterministic;
+% export_fig(gcf,'./plots/p_plots_GR20_deterministic.pdf');
+% close all;
 
 p_values_plot_subspace_expected;
 export_fig(gcf,'./plots/p_plots_GR20_subspace_expected.pdf');
