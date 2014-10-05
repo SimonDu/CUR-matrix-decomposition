6659953c4afe43e624af38dd62e397a3845fe5d8
Simon Shaolei Du <simonshaoleidu@gmail.com>
2014-10-04 20:50:16 -0700

    add GR20

---
+++ algorithms_comparision_experiments/compare_GR20.m
@@ -0,0 +1,25 @@
+in.A = read_snap_data('CA-GrQc.txt');
+
+in.k = 20;
+in.p = 20;
+in.q = 15;
+
+in.sigma_k = 1;
+in.froerr = 1;
+in.froerr_k = 1;
+in.specerr = 0;
+in.specerr_k = 0;
+
+c_values = (40:10:150);
+r_values = (40:10:150);
+
+
+methods = {'deterministic','subspace_expected','subspace_approxlevscore_gaussian','subspace_approxlevscore_powermethod','uniform'};
+out = run_different_number_of_c_and_r(in,methods,c_values,r_values);
+
+
+save('./output/compare_GR20')
+
+c_values_plot;
+export_fig(gcf,'./plots/c_plots_GR20.pdf');
+close all;
