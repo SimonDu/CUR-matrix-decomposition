7b611d6f538b3f21b79b0023fb9951123bee38e3
Simon Shaolei Du <simonshaoleidu@gmail.com>
2014-10-05 15:56:07 -0700

    add randomized_unweighted

--- algorithms/randomized_unweighted.m
+++ algorithms/randomized_unweighted.m
@@ -1,4 +1,4 @@
-function out = subspace_expected(in)
+function out = randomized_unweighted(in)
 %
 % in is a structure with (at least) the following fields:
 % - A, a matrix
@@ -13,7 +13,7 @@ function out = subspace_expected(in)
 % - froerr_k, 1 if we want output contain froerr_k, 0 otherwise
 % - specerr, 1 if we want output contain specerr, 0 otherwise
 % - specerr_k, 1 if we want output contain specerr_k, 0 otherwise
-%
+
 % out is a structure with the following fields:
 %  - cidx, c*q matrix represents the column index we choose for each
 %  iteration
@@ -33,6 +33,7 @@ function out = subspace_expected(in)
 %  - metric_computing_time: 1*q vector represents the time to compute
 %  different metrics
 
+k = in.k;
 c = in.c;
 r = in.r;
 p = in.p;
@@ -59,50 +60,63 @@ end
 
 out.construct_time = zeros(1,q);
 out.metric_computing_time = zeros(1,q);
+[m,n] = size(in.A);
 
-
-for iter=1:in.q
-    tic
-        [Ua,~,Va]=svds(in.A,in.p);
-        out.cidx{iter} = CX_SubspaceExpected(Va, p, c);
-        C = in.A(:,out.cidx{iter});
-        
-        out.ridx{iter} = CX_SubspaceExpected(Ua, p, r);
-        R = in.A(out.ridx{iter},:);
-
+for iter = 1:q
+    tic;
+    Omega = randn(n,4*p);
+    Y = in.A*Omega;
+    for i = 1:1
+        Y = in.A*(in.A'*Y);
+    end
+    [~,~,Va] = svds(Y,p);
+    out.cidx{iter} = MSelect(Va(:,1:p),p,c);
+    C = in.A(:,out.cidx{iter});
+    
+    Omega = randn(m,4*p);
+    AT = in.A';
+    Y = AT*Omega;
+    for i = 1:1
+        Y = AT*(AT'*Y);
+    end
+    [~,~,Va]=svds(Y,p);
+    out.ridx{iter} = MSelect(Va(:,1:p),p,r);
+    R = in.A(out.ridx{iter},:);
+    
     out.construct_time(1,iter) = toc;
     
     tic
-        [Qc,~] = qr(C,0);
-        [Qr,~] = qr(R',0);
+    [Qc,~] = qr(C,0);
+    [Qr,~] = qr(R',0);
     
-        B = Qc'*in.A*Qr;
-        CUR = Qc*B*Qr';
-        [Ub,Sb,Vb] = svds(B,in.k);
-        Bk = Ub*Sb*Vb';
-        CUR_k = Qc*Bk*Qr';
+    B = Qc'*in.A*Qr;
+    CUR = Qc*B*Qr';
+    [Ub,Sb,Vb] = svds(B,in.k);
+    Bk = Ub*Sb*Vb';
+    CUR_k = Qc*Bk*Qr';
     
-        residual = in.A-CUR;
-        residual_k = in.A - CUR_k;
+    residual = in.A-CUR;
+    residual_k = in.A - CUR_k;
     
-        if(in.sigma_k)
-            out.sigma_k(1,iter) = Sb(end,end);
-        end
-        if(in.froerr)
-            out.froerr(1,iter) = norm(residual,'fro');
-        end
-        if(in.froerr_k)
-            out.froerr_k(1,iter) = norm(residual_k,'fro');
-        end
-        if(in.specerr)
-            out.specerr(1,iter) = svds(residual,1);
-        end
-        if(in.specerr_k)
-            out.specerr_k(1,iter) = svds(residual_k,1);
-        end
+    if(in.sigma_k)
+        out.sigma_k(1,iter) = Sb(end,end);
+    end
+    if(in.froerr)
+        out.froerr(1,iter) = norm(residual,'fro');
+    end
+    if(in.froerr_k)
+        out.froerr_k(1,iter) = norm(residual_k,'fro');
+    end
+    if(in.specerr)
+        out.specerr(1,iter) = svds(residual,1);
+    end
+    if(in.specerr_k)
+        out.specerr_k(1,iter) = svds(residual_k,1);
+    end
+    
     out.metric_computing_time(1,iter) = toc;
-
 end
+
 
 end
 
