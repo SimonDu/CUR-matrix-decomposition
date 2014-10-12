b9fe3a73ef7ffa26eb57f74d41044cabd585a6c1
Simon Shaolei Du <simonshaoleidu@gmail.com>
2014-10-12 01:36:23 -0700

    add more scripts

---
+++ compare_gisette10.m
@@ -0,0 +1,28 @@
+load('gisette_train.data');
+in.A = gisette_train;
+
+
+in.k = 10;
+in.p = 10;
+in.q = 10;
+
+in.sigma_k = 1;
+in.froerr = 1;
+in.froerr_k = 1;
+in.specerr = 0;
+in.specerr_k = 0;
+
+c_values = (20:20:140);
+r_values = (20:20:140);
+
+
+methods = {'deterministic','randomized_unweighted','subspace_expected','subspace_approxlevscore_gaussian','uniform_sampling','near_optimal'};
+out = run_different_number_of_c_and_r(in,methods,c_values,r_values);
+
+
+save('./output/compare_gisette10')
+
+c_values_plot;
+saveas(gcf,'./plots/c_plots_gisette10','fig');
+export_fig(gcf,'./plots/c_plots_gisette10.pdf');
+close all;
---
+++ compare_wine10_sigma_2.m
@@ -0,0 +1,30 @@
+in.sigma = 2;
+load 'winequality_data';
+X = generate_distance_matrix(X);
+in.A = generate_RBF_kernel(X, in.sigma);
+clear X;
+
+in.k = 10;
+in.p = 19;
+in.q = 10;
+
+in.sigma_k = 1;
+in.froerr = 1;
+in.froerr_k = 1;
+in.specerr = 0;
+in.specerr_k = 0;
+
+c_values = (in.p+1:20:140);
+r_values = (in.p+1:20:140);
+
+
+methods = {'deterministic','randomized_unweighted','subspace_expected','subspace_approxlevscore_gaussian','uniform_sampling','near_optimal'};
+out = run_different_number_of_c_and_r(in,methods,c_values,r_values);
+
+
+save('./output/compare_wine10_sigma_2');
+
+c_values_plot;
+saveas(gcf,'./plots/c_plots_wine_10_sigma_2','fig');
+export_fig(gcf,'./plots/c_plots_wine_10_sigma_2.pdf');
+close all;
