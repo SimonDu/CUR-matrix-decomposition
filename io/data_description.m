ae13bd77b18fa7f35139c94ccc629e200982c30d
kenny0805 <gdgzwkm@gmail.com>
2014-10-14 21:32:44 -0700

    update data description

--- io/data_description.m
+++ io/data_description.m
@@ -1,7 +1,15 @@
 [m,n] = size(in.A)
-nnz(in.A)/(m*n)
+100*nnz(in.A)/(m*n)
 k = 10
 [U,S,V] = svds(in.A,k);
+A_k = U*S*V';
+
+f_norm = norm(in.A,'fro');
+two_norm = svds(in.A,1);
+
+ceil(f_norm/two_norm)
+100*norm(in.A-A_k,'fro')/norm(in.A,'fro')
+
 for i=1:m
     L(i) = norm(U(i,:));
 end
@@ -11,3 +19,4 @@ for i=1:n
     L(i) = norm(V(i,:));
 end
 mu_r = n/k * max(L)
+
