4b78feb09a08575af79a40c82d401c36722b88f6
Simon Shaolei Du <simonshaoleidu@gmail.com>
2014-10-21 02:26:52 -0700

    add data_info

--- io/data_info.m
+++ io/data_info.m
@@ -1,14 +1,15 @@
 [m,n] = size(in.A)
-100*nnz(in.A)/(m*n)
+nnz_ratio = 100*nnz(in.A)/(m*n)
 k = in.k
 [U,S,V] = svds(in.A,k);
 A_k = U*S*V';
 
+
 f_norm = norm(in.A,'fro');
 two_norm = svds(in.A,1);
 
-ceil((f_norm/two_norm)^2)
-100*norm(in.A-A_k,'fro')/norm(in.A,'fro')
+f_2_ratio = (f_norm/two_norm)^2
+A_k_left_ratio = 100*norm(in.A-A_k,'fro')/norm(in.A,'fro')
 
 for i=1:m
     L(i) = norm(U(i,:));
@@ -20,3 +21,5 @@ for i=1:n
 end
 mu_r = n/k * max(L)
 
+s = svds(in.A,2*k);
+sigma_k_ratio = s(2*k)/s(k)
